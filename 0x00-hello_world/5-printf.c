19329985d69830b87bf43134b7de19fcbd329a76
girmesh03 <girmachewzewdie03@gmail.com>
2022-10-13 08:34:59 -0700

    file is added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - The function puts
+ *
+ * Return: 0 (On Success)
+ */
+int main(void)
+{
+	printf("with porper grammer, but the outcome is piece of art,\n");
+	return (0);
+}

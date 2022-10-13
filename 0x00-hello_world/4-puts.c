6695c3b789ce3ca785a482c9bbedef6601f2fbbc
girmesh03 <girmachewzewdie03@gmail.com>
2022-10-13 08:19:09 -0700

    file added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - The function puts
+ *
+ * Return: 0 (On Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}

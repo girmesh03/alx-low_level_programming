2ac17c9d55fdd965401f57078d4f220ba39e1acc
girmesh03 <girmachewzewdie03@gmail.com>
2022-10-14 04:46:35 -0700

    betty space fixed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,8 @@
 
 /**
  * main - Function
- * 
- * Description: Checks munber n
- *
+ * Description: Checks number n
  * Return: 0
- *
  */
 
 int main(void)
@@ -14,21 +11,26 @@ int main(void)
 	int n;
 
 	srnad(time(0));
-	n = rand() - RAND_MAX / 2 ;
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
+
 		printf("%d is positive\n", n);
+
 	}
-	else if (n == 0)	
+	else if (n == 0)
 	{
+
 		printf("%d is zero\n", n);
+
 	}
 	else
 	{
+
 		printf("%d is negative\n", n);
+
 	}
-	
 	return (0);
 }
 

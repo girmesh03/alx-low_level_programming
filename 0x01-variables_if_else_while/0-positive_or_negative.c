43a5602dd762c47048473d68239f213ed1a8889c
girmesh03 <girmachewzewdie03@gmail.com>
2022-10-14 04:22:54 -0700

    first commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+
+/**
+ * main - Function
+ * 
+ * Description: Checks munber n
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	srnad(time(0));
+	n = rand() - RAND_MAX / 2 ;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)	
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	
+	return (0);
+}
+
+

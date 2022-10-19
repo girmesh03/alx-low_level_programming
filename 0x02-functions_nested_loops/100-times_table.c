38f55070a84e74700a0da8fa3de5c73bdf1f0aa5
girmesh03 <girmachewzewdie03@gmail.com>
2022-10-19 10:57:04 -0700

    betty bug fixed

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,48 @@
 #include "main.h"
 /**
- * print_times_table - print time table
- * @n: n's table n < 15 and n > 0
+ * print_times_table - prints n times table
+ * @n: n's table b/n n > 0 annd n <= 15
  * Returns: void
  */
-
 void print_times_table(int n)
 {
 	int number;
 	int multiplier;
 	int result;
 
-	if ((n > 0) && (n < 15))
-	{
-		for (multiplier = 0; multiplier <= n; multiplier++)
+	if (n < 0 || n >= 15)
+		return;
+	for (number = 0; number <= n; number++)
 		{
-			for (number = 0; number <= n; number++)
+		for (multiplier = 0; multiplier <= n; multiplier++)
 			{
-				result = number * multiplier;
-				if (number == 0)
+			result = (number * multiplier);
+			if (multiplier == 0)
+				_putchar(48 + result);
+			else
 				{
-					_putchar(result + 48);
-				}
-				else
-				{
-					_putchar(',');
+				_putchar(',');
+				_putchar(' ');
+				if (result <= 9)
+					{
 					_putchar(' ');
-					if (result <= 9)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(result + 48);
+					_putchar(' ');
+					_putchar(48 + result);
 					}
-					else if ((result > 9) && (result < 100))
+				else if (result > 9 && result < 100)
 					{
-						_putchar(' ');
-						_putchar((result / 10) + 48);
-						_putchar((result % 10) + 48);
+					_putchar(' ');
+					_putchar(48 + (result / 10));
+					_putchar(48 + (result % 10));
 					}
-					else if (result >= 100)
+				else if (result >= 100)
 					{
-						_putchar((result / 100) + 48);
-						_putchar(((result / 10) % 10) + 48);
-						_putchar((result % 10) + 48);
+					_putchar(48 + (result / 100));
+					_putchar(48 + ((result / 10) % 10));
+					_putchar(48 + (result % 10));
 					}
 				}
 			}
 		_putchar('\n');
 		}
-	}
 }

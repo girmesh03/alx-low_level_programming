7625a551b4238059f45a1c708128c7316fdf3553
girmesh03 <girmachewzewdie03@gmail.com>
2022-10-19 11:32:56 -0700

    102-fibonacci.c file is added

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints fibonacci numbers starting from 1
+ * Return: On successs (0)
+ */
+
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+	int result;
+
+	for (int i = 1; i <= 50; i++)
+		{
+		printf("%d", a);
+		if (i < 50)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		result = a + b;
+		a = b;
+		b = result;
+
+		}
+	printf("\n");
+	return (0);
+}

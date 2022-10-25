e42ab728bc92d2f694d9cfd8c75422031eaa97fe
girmesh03 <girmachewzewdie03@gmail.com>
2022-10-24 17:08:51 -0700

    print array of integer

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Updates the value it points to to 98.
+ * @a: Pointer.
+ * @n: The number of elements of the array to be printed.
+ * Return: void.
+ */
+
+void print_array(int *a, int n)
+{
+	int index;
+	int i;
+	int j;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = 0; j < n - 1; j++)
+		{
+			if (a[j + 1] > a[j])
+			{
+				int tmp = a[j];
+
+				a[j] = a[j + 1];
+				a[j + 1] = tmp;
+			}
+		}
+	}
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d, ", *(a + index));
+	}
+
+	printf("\n");
+}
+

86141506259930234f622c965385dbc8ba4892b1
girmesh03 <girmachewzewdie03@gmail.com>
2022-10-31 12:13:39 -0700

    from main.h putchar prototype is removed

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
-
-int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -13,6 +11,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
+int _atoi(char *s);
 
 #endif

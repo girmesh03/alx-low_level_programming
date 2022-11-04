cd6252976dbaf35b486f0ff520aafce01f15f047
girmesh03 <girmachewzewdie03@gmail.com>
2022-11-04 13:57:19 -0700

    for loop is addede

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,7 +3,7 @@
 #include <ctype.h>
 
 /**
- * main - Prints the number of arguments passed into it.
+ * main - Adds the arguments passed into it.
  * @argc: Number of command line arguments.
  * @argv: Array that contains the program command line arguments.
  * Return: 0 (Success) and 1 (Error).
@@ -11,19 +11,21 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!isdigit(argv[i][0]))
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+
 		}
-		else
-		{
-			sum += atoi(argv[i]);
-		}
+
+		sum += atoi(argv[i]);
 	}
 
 	printf("%d\n", sum);

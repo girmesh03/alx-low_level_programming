b510cbe480a3f3176804ea0775296ea56b1cf8a3
girmesh03 <girmachewzewdie03@gmail.com>
2022-11-14 20:38:19 +0300

    omputes the absolute value of a number

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ *
+ * ABS(x) - Computes absolute value of x
+ * @x: Any integer.
+ * Description: computes the absolute value of a number x.
+ *
+ */
+
+#define ABS(x) ((x < 0) ? -(x) : x)
+
+#endif

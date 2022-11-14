3b462d16976a1a13a57b89009b8f7568effd1274
girmesh03 <girmachewzewdie03@gmail.com>
2022-11-14 19:49:01 +0300

    defines a macro named SIZE of token

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * File: 0-object_like_macro.h
+ * Description: defines a macro named SIZE
+ *		as an abbreviation for the token 1024.
+ *
+ */
+
+/* Buffer size of token */
+#define SIZE 1024
+
+#endif

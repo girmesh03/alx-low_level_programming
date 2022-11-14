c65735b9a05523b859bcf09357f50496b4fb7f29
girmesh03 <girmachewzewdie03@gmail.com>
2022-11-14 20:44:11 +0300

    macro expression bracket is added

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,13 +2,12 @@
 #define FUNCTION_LIKE_MACRO_H
 
 /**
- *
  * ABS(x) - Computes absolute value of x
  * @x: Any integer.
  * Description: computes the absolute value of a number x.
  *
  */
 
-#define ABS(x) ((x < 0) ? -(x) : x)
+#define ABS(x) (((x < 0)) ? -(x) : (x))
 
 #endif

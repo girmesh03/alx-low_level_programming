c62f950393210063850f4d1c10e4275fb6bcd35a
girmesh03 <girmachewzewdie03@gmail.com>
2022-11-14 20:52:29 +0300

    omputes the sum of the numbers x and y

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * SUM(x, y) - Function like macro.
+ * @x: The first number.
+ * @y: The second number.
+ * Description: Computtes the sum of the numbers x and y.
+ * Return: Sum.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif

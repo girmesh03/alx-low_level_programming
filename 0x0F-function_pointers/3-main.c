1e54baed7db40c83fbc1e3b63ff0311916a62c43
girmesh03 <girmachewzewdie03@gmail.com>
2022-11-17 06:12:26 +0300

    hot fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,39 +1,47 @@
+
 #include <stdio.h>
 #include "3-calc.h"
 #include <stdlib.h>
-/**
- * main - Prints the result of simple operations.
- * @argc: Number of argument.
- * @argv: A pointer to a pointer string input.
- * Return: 0 on success or 98 on error.
- *
- */
-int main(int argc, char **argv)
-{
-	int first_number = atoi(argv[1]);
-	int second_number = atoi(argv[3]);
-	char *select_operator;
 
-	select_operator = argv[2];
+/**
+* main - Prints the result of simple operations.
+* @argc: Number of argument.
+* @argv: A pointer to a pointer string input.
+* Return: 0 on success or 98 on error.
+*
+*/
+
+int main(int argc, char *argv[])
+{
+	int first_number;
+	int second_number;
+	char *select;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	else if (get_op_func(select_operator) == NULL)
+
+	first_number = atoi(argv[1]);
+	select = argv[2];
+	second_number = atoi(argv[3]);
+
+	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	else if ((*select_operator == '/' && second_number == 0) ||
-			(*select_operator == '%' && second_number == 0))
+
+	if ((*select == '/' && second_number == 0) ||
+			(*select == '%' && second_number == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", get_op_func(select_operator)(first_number,
-				second_number));
+
+	printf("%d\n", get_op_func(argv[2])(first_number, second_number));
+
 	return (0);
 }
 

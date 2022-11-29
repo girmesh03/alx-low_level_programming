aae2479ba5b8094771d2cf183dd61d4fbc8c5944
girmesh03 <girmachewzewdie03@gmail.com>
2022-11-29 22:56:23 +0300

    code changed

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,85 +1,81 @@
 #include "lists.h"
 #include <stdio.h>
 
-size_t count_node(const listint_t *head);
-
 /**
- * print_listint_safe - prints a listint_t linked list.
- * @head: pointer to the head of the list
- * Return: the number of nodes in the list
+ * count_count - Counts the number of unique counts in a list
+ * @head: A pointer to the head of the listint_t to check.
+ * Return: If the list is not looped - the number of nodes in the list.
  */
-size_t print_listint_safe(const listint_t *head)
-{
-	size_t i = 0, len = 0;
-	const listint_t *temp;
-
-	if (head == NULL)
-		return (0);
-
-	len = count_node(head);
-
-	if (len == 0)
-	{
-		while (head != NULL)
-		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
-			i++;
-		}
-	}
-	else
-	{
-		temp = head;
-		while (len != 0)
-		{
-			printf("[%p] %d\n", (void *)temp, temp->n);
-			temp = temp->next;
-			len--;
-			i++;
-		}
-		printf("-> [%p] %d\n", (void *)temp, temp->n);
-		i++;
-	}
-
-	return (i);
-}
-
-/**
- * count_node - counts the number of nodes in a listint_t list.
- * @head: pointer to the head of the list
- * Return: the number of nodes in the list
- */
-size_t count_node(const listint_t *head)
+size_t count_count(const listint_t *head)
 {
 	const listint_t *slow, *fast;
-	size_t len = 0;
+	size_t count = 1;
 
-	if (head == NULL)
+	if (head == NULL || head->next == NULL)
 		return (0);
 
-	slow = head;
-	fast = head->next;
+	slow = head->next;
+	fast = (head->next)->next;
 
-	while (fast != NULL && fast->next != NULL)
+	while (fast)
 	{
 		if (slow == fast)
 		{
-			len = 1;
-			break;
-		}
-		slow = slow->next;
-		fast = fast->next->next;
-	}
+			slow = head;
+			while (slow != fast)
+			{
+				count++;
+				slow = slow->next;
+				fast = fast->next;
+			}
 
-	if (len == 1)
-	{
-		slow = slow->next;
-		while (slow != fast)
-		{
 			slow = slow->next;
-			len++;
+			while (slow != fast)
+			{
+				count++;
+				slow = slow->next;
+			}
+
+			return (count);
+		}
+
+		slow = slow->next;
+		fast = (fast->next)->next;
+	}
+
+	return (0);
+}
+
+/**
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ * Return: The number of count in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count, index = 0;
+
+	count = count_count(head);
+
+	if (count == 0)
+	{
+		for (; head != NULL; count++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
 	}
 
-	return (len);
+	else
+	{
+		for (index = 0; index < count; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+
+	return (count);
 }

7a59e4f3592a7da6bceaf52ec4212baade9de1de
girmesh03 <girmachewzewdie03@gmail.com>
2023-01-12 20:34:47 +0300

    code updated

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,27 +1,40 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position
+ * insert_dnodeint_at_index - inserts a new_node node at a given position
  * @h: pointer to head of list
- * @idx: is the index of the list where the new node should be added.
+ * @idx: is the index of the list where the new_node node should be added.
  * Index starts at 0
- * @n: data for new node
- * Return: the address of the new node, or NULL if it failed
- * if it is not possible to add the new node at index idx,
- * do not add the new node and return NULL
+ * @n: data for new_node
+ * Return: the address of the new_node, or NULL if it failed
+ * if it is not possible to add the new_node at index idx,
+ * do not add the new_node and return NULL
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	/* declare variables */
-	dlistint_t *new_node, *temp;
-	unsigned int i;
+	/* declare temp pointer and set to head */
+	/* declare new_node pointer */
+	dlistint_t *tmp = *h, *new_node;
 
-	/* if head is NULL, return NULL */
-	if (h == NULL)
-		return (NULL);
+	/* if idx is 0, call add_dnodeint */
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-	/* allocate memory for new node */
+	/* traverse list until idx is not equal to 1 */
+	for (; idx != 1; idx--)
+	{
+		/* if tmp is NULL, return NULL */
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+	}
+
+	/* if tmp->next is NULL, call add_dnodeint_end */
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	/* allocate memory for new_node */
 	new_node = malloc(sizeof(dlistint_t));
 	/* if malloc fails, return NULL */
 	if (new_node == NULL)
@@ -29,52 +42,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	/* set new_node->n to n */
 	new_node->n = n;
-	/* set new_node->next to NULL */
-	new_node->next = NULL;
-	/* set new_node->prev to NULL */
-	new_node->prev = NULL;
-
-	/* if idx is 0, add new node at beginning of list */
-	if (idx == 0)
-	{
-		/* if head is NULL, set new_node->next to NULL */
-		if (*h == NULL)
-			new_node->next = NULL;
-		/* else, set new_node->next to head */
-		else
-			new_node->next = *h;
-		/* set head to new_node */
-		*h = new_node;
-		/* return new_node */
-		return (new_node);
-	}
-
-	/* set temp to head */
-	temp = *h;
-	/* traverse list until temp is NULL */
-	for (i = 0; temp != NULL; i++)
-	{
-		/* if i is equal to idx, insert new node */
-		if (i == idx)
-		{
-			/* set new_node->next to temp */
-			new_node->next = temp;
-			/* set new_node->prev to temp->prev */
-			new_node->prev = temp->prev;
-			/* set temp->prev->next to new_node */
-			temp->prev->next = new_node;
-			/* set temp->prev to new_node */
-			temp->prev = new_node;
-			/* return new_node */
-			return (new_node);
-		}
-		/* temp points to next node */
-		temp = temp->next;
-	}
-
-	/* if i is not equal to idx, return NULL */
-	if (i != idx)
-		return (NULL);
+	/* set new_node->next to tmp->next */
+	new_node->next = tmp->next;
+	/* set new_node->prev to tmp */
+	new_node->prev = tmp;
+	/* set tmp->next->prev to new_node */
+	tmp->next->prev = new_node;
+	/* set tmp->next to new_node */
+	tmp->next = new_node;
 
 	/* return new_node */
 	return (new_node);

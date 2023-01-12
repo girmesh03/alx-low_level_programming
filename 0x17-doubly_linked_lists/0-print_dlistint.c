9d0c20a1acbf9480649b17eb7abce47c46d5cf08
girmesh03 <girmachewzewdie03@gmail.com>
2023-01-12 18:33:03 +0300

    changed declering head ptr method

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,7 +11,8 @@ size_t print_dlistint(const dlistint_t *h)
 	/* declare a variable to count the number of nodes */
 	size_t num_nodes = 0;
 	/* current is a pointer to the head of the list */
-	dlistint_t *current = (dlistint_t *)h;
+	/* dlistint_t *current = (dlistint_t *)h; */
+	const dlistint_t *current = h;
 
 	/* while current is not NULL, print the value of the node, */
 	/* count the number of nodes, and move to the next node */

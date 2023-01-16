5ac0bf1c07f8203d855ce5002bd2af9a662b9290
girmesh03 <girmachewzewdie03@gmail.com>
2023-01-17 01:23:27 +0300

    code changed

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/girmesh03/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P /tmp/ https://raw.github.com/girmesh03/alx-low_level_programming/master/0x18-dynamic_libraries/101-make_me_win.sh
+export LD_PRELOAD=/tmp/101-make_me_win.sh

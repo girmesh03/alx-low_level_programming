11e1e113812b237f1cd50908d7c51ac1493bd72f
girmesh03 <girmachewzewdie03@gmail.com>
2023-01-20 14:58:57 +0300

    101-make_me_win.sh

--- 0x18-dynamic_libraries/nrandom.c
+++ 0x18-dynamic_libraries/nrandom.c
@@ -5,6 +5,6 @@
 int printf(const char *format, ...)
 {
 	write(1, "64 55 17 39 11 - 9\n", 17);
-	write(1, "Congratulations, you win the Jackpot!\n", 16);
+	write(1, "Congratulations, you win the Jackpot!\n", 38);
 	exit(EXIT_SUCCESS);
 }

1f46c1d668392913895b8b10d98f0a3c48b0d8e6
girmesh03 <girmachewzewdie03@gmail.com>
2023-01-20 13:44:02 +0300

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/jimmydaniel/alx-low_level_programming/master/0x18-dynamic_libraries/101-make_me_win.sh 
+wget https://raw.githubusercontent.com/girmesh03/alx-low_level_programming/master/0x18-dynamic_libraries/101-make_me_win.sh

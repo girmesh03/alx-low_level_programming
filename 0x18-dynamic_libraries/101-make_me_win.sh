06aa43b286baa759b13f3fd91f4a219b2dd772b8
girmesh03 <girmachewzewdie03@gmail.com>
2023-01-20 13:39:07 +0300

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/girmesh03/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget https://raw.githubusercontent.com/jimmydaniel/alx-low_level_programming/master/0x18-dynamic_libraries/101-make_me_win.sh 
+

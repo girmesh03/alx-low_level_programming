171a2e0f5420db87b53db35946ad59161d6ba176
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-10 05:50:56 +0300

    directroy cleaned

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,13 +7,12 @@
 
 /**
  * struct listint_s - singly linked list
- *
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
- *
  * Description: singly linked list node structure
  */
+
 typedef struct listint_s
 {
 	int n;
@@ -23,14 +22,13 @@ typedef struct listint_s
 
 /**
  * struct skiplist_s - Singly linked list with an express lane
- *
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
  * @express: Pointer to the next node in the express lane
- *
  * Description: singly linked list node structure with an express lane
  */
+
 typedef struct skiplist_s
 {
 	int n;
